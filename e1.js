6dcb3da9049919693b89df1a96ff5490da7f9614
prabin <bhandariprabin402@gmail.com>
2023-12-27 18:22:29 +0545

    add e1

---
+++ e1.js
@@ -0,0 +1,10 @@
+function numberkk(a, b) {
+  if (Number(b) === 0) {
+    console.log("error");
+    return false;
+  } else {
+    console.log("number is valid");
+    return true;
+  }
+}
+numberkk(2, 0);
